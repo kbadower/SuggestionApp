307df932a9bf3e1d9a7d6081ce13237432fae340
Krzysztof Badower <kbadower@gmail.com>
2023-11-01 23:54:33 +0100

    Add User Data access

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,5 +1,35 @@
-﻿namespace SuggestionAppLibrary.DataAccess;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
-public class DbConnection
+namespace SuggestionAppLibrary.DataAccess;
+
+public class DbConnection : IDbConnection
 {
+    private IConfiguration _configuration;
+    private IMongoDatabase _db;
+    private readonly string _connectionId = "MongoDB";
+    public string DbName { get; private set; }
+    public string CategoryCollectionName { get; private set; } = "categories";
+    public string StatusCollectionName { get; private set; } = "statuses";
+    public string UserCollectionName { get; private set; } = "users";
+    public string SuggestionCollectionName { get; private set; } = "suggestions";
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
+
+    public DbConnection(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Client = new MongoClient(_configuration.GetConnectionString(_connectionId));
+        DbName = _configuration["DatabaseName"];
+        _db = Client.GetDatabase(DbName);
+
+        CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+        SuggestionCollection = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
+    }
+
 }
--- SuggestionAppLibrary/Models/UserModel.cs
+++ SuggestionAppLibrary/Models/UserModel.cs
@@ -4,7 +4,7 @@ public class UserModel
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string ObjectIdentifier { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
