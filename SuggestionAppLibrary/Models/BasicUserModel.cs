d1a9999ed0aa8d768efc9a467f6d9d9fe2b215b8
Krzysztof Badower <kbadower@gmail.com>
2023-11-01 19:40:29 +0100

    Add base models

---
+++ SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class BasicUserModel
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    public string DisplayName { get; set; }
+
+    public BasicUserModel()
+    {
+        
+    }
+
+    public BasicUserModel(UserModel user)
+    {
+        DisplayName = user.DisplayName;
+    }
+}
--- SuggestionAppLibrary/Models/SuggestionModel.cs
+++ SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -2,4 +2,20 @@
 
 public class SuggestionModel
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    public string Suggestion { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    public CategoryModel Category { get; set; }
+    public string Author { get; set; }
+    public HashSet<string> UserVotes { get; set; } = new();
+    public StatusModel Status { get; set; }
+    public string OwnerNotes { get; set; }
+    public bool IsApprovedForRelease { get; set; } = false;
+    public bool IsArchived { get; set; } = false;
+    public bool MyProperty { get; set; } = false;   
 }
+
+
