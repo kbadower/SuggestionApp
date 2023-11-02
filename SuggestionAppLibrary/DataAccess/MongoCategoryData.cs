ea8ebca8fbee811b3650a9c3f6d86f532fdc439e
Krzysztof Badower <kbadower@gmail.com>
2023-11-02 18:37:42 +0100

    Add category data access

---
+++ SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoCategoryData : ICategoryData
+{
+    private IMongoCollection<CategoryModel> _categories;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "CategoryData";
+
+    public MongoCategoryData(IDbConnection db, IMemoryCache cache)
+    {
+        _categories = db.CategoryCollection;
+        _cache = cache;
+    }
+
+    public async Task<List<CategoryModel>> GetCategoriesAsync()
+    {
+        var output = _cache.Get<List<CategoryModel>>(cacheName);
+
+        if (output is null)
+        {
+            var result = await _categories.FindAsync(_ => true);
+            output = result.ToList();
+
+            _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+        }
+
+        return output;
+    }
+
+    public Task CreateCategory(CategoryModel category)
+    {
+        return _categories.InsertOneAsync(category);
+    }
+
+}
