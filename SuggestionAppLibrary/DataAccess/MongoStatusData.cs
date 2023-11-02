e0e2c3ab25751db4c3e54677cee688c9564f0440
Krzysztof Badower <kbadower@gmail.com>
2023-11-02 18:46:09 +0100

    Add status data access

---
+++ SuggestionAppLibrary/DataAccess/MongoStatusData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoStatusData : IStatusData
+{
+    private readonly IMongoCollection<StatusModel> _statuses;
+    private IMemoryCache _cache;
+    private const string cacheName = "StatusData";
+
+    public MongoStatusData(IDbConnection db, IMemoryCache cache)
+    {
+        _statuses = db.StatusCollection;
+        _cache = cache;
+    }
+
+    public async Task<List<StatusModel>> GetStatusesAsync()
+    {
+        var output = _cache.Get<List<StatusModel>>(cacheName);
+
+        if (output is null)
+        {
+            var result = await _statuses.FindAsync(_ => true);
+            output = result.ToList();
+            _cache.Set(cacheName, output);
+        }
+
+        return output;
+    }
+
+    public Task CreateStatus(StatusModel status)
+    {
+        return _statuses.InsertOneAsync(status);
+    }
+}
