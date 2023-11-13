606f491256834274d88a053975de0cc5d0e4b44e
Krzysztof Badower <kbadower@gmail.com>
2023-11-13 23:00:25 +0100

    Handle actions of authenticated user
    *Get user data on pages
    *Update user data in db
    *Voting
    *Access to pages
    *Fix transaction bug in suggestion data access

---
+++ SuggestionAppUI/Helpers/AuthenticationStateProviderHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace SuggestionAppUI.Helpers
+{
+    public static class AuthenticationStateProviderHelpers
+    {
+        public static async Task<UserModel> GetUserFromAuthentication(this AuthenticationStateProvider authProdiver, IUserData userData)
+        {
+            var authState = await authProdiver.GetAuthenticationStateAsync();
+            string objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+            return await userData.GetUserFromAuthentication(objectId);
+        }
+    }
+}
